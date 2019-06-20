7b8470cac33a790c742c26e01327c33a89f5cdea
Loïc de Wit <dew.loic@gmail.com>
2019-06-20 10:59:31 +0200

    load studens from file; resolved couple bugs

--- directory.rb
+++ directory.rb
@@ -10,7 +10,7 @@ def input_students
   while !name.empty? do
     # add the student hash to the array
     @students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} students"
+    puts "Now we have #{@students.count} students"
     # get another name from the user
     name = gets.chomp
   end
@@ -38,6 +38,8 @@ end
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
+  puts "3. Save the list to students.csv"
+  puts "4. Load the list from students.csv"
   puts "9. Exit"
 end
 def show_students
@@ -51,11 +53,34 @@ def process(selection)
     students = input_students
   when "2"
     show_students
+  when "3"
+    save_students
+  when "4"
+    load_students
   when "9"
     exit # causes program to terminate
   else
     puts "I don't know what you meant, try again"
   end
+end
+def save_students
+  # open the file for writing
+  file = File.open("students.csv", "w")
+  # iterate over the array of students
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
+end
+def load_students
+  file = File.open("students.csv", "r")
+  file.readlines.each do |line|
+    name, cohort = line.chomp.split(",")
+    @students << {name: name, cohort: cohort.to_sym}
+  end
+  file.close
 end
 
 interactive_menu
